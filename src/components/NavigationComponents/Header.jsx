bdd036a86ab78a7a4e99ba9005b019f5c94b6218
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-09-27 15:40:04 +0530

    working-on-header

--- src/components/NavigationComponents/Header.jsx
+++ src/components/NavigationComponents/Header.jsx
@@ -3,47 +3,57 @@
 import { headerdData } from '@/constants/Aspirants/aspirantPageData'
 import {
   AppBar,
+  useScrollTrigger,
   Box,
   Container,
-  List,
-  ListItem,
+  Slide,
   Stack,
   Toolbar,
 } from '@mui/material'
-import Link from 'next/link'
-import MenuButton from '../buttonComponents/MenuButton'
-import NavButton from '../buttonComponents/NavButton'
 import CustomImage from '../globalComponents/CustomImage/CustomImage'
 import Navigation from './Navigation'
-function Header() {
+const HideOnScroll = function ({ window, children }) {
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  })
+  console.log(trigger)
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  )
+}
+function Header(props) {
   return (
     <Box component="header">
-      <AppBar
-        component="nav"
-        sx={{
-          backgroundColor: 'primaryPalette.white',
-          boxShadow: 'unset',
-          zIndex: '1200',
-        }}
-      >
-        <Toolbar>
-          <Container>
-            <Stack
-              alignItems="center"
-              justifyContent="space-between"
-              flexDirection="row"
-            >
-              <CustomImage
-                src={headerdData.logo}
-                alt="Razorswift-logo"
-                width="clamp(160px,10vw,244px)"
-                aspectRatio="2/1"
-              />
-              <Navigation headerdData={headerdData} />
-            </Stack>
-          </Container>
-        </Toolbar>
-      </AppBar>
+      <HideOnScroll {...props}>
+        <AppBar
+          component="nav"
+          sx={{
+            backgroundColor: 'primaryPalette.white',
+            boxShadow: 'unset',
+            zIndex: '1200',
+          }}
+        >
+          <Toolbar>
+            <Container>
+              <Stack
+                alignItems="center"
+                justifyContent="space-between"
+                flexDirection="row"
+              >
+                <CustomImage
+                  src={headerdData.logo}
+                  alt="Razorswift-logo"
+                  width="clamp(160px,10vw,244px)"
+                  aspectRatio="2/1"
+                />
+                <Navigation headerdData={headerdData} />
+              </Stack>
+            </Container>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
     </Box>
   )
 }
