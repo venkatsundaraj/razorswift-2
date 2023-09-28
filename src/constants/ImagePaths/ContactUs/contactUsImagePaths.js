2fd508b63d49f7d044940f95c6392186fea53193
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-09-28 18:14:36 +0530

    before-contact

--- src/constants/ImagePaths/ContactUs/contactUsImagePaths.js
+++ src/constants/ImagePaths/ContactUs/contactUsImagePaths.js
@@ -1,6 +1,5 @@
 const contactUsImagePaths = {
-  image1: require('/public/images/Aspirants/get-verified-get-hired-1.webp')
-    .default,
+  image1: require('/public/images/ContactUs/contact-us-banner.svg').default,
   aboutIcon: require('/public/images/ContactUs/join-us-vector.9b5375c9.svg')
     .default,
 }
