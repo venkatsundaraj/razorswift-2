d9522c4026356af8665ecace9d81741fba400fe3
firebrand-labs <vimalraj@firebrandlabs>
2023-10-03 16:26:30 +0530

    over

--- src/components/pageBasedComponents/Homepage/DemoSection.jsx
+++ src/components/pageBasedComponents/Homepage/DemoSection.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import { Box } from '@mui/system'
 import { Typography } from '@mui/material'
@@ -5,8 +6,28 @@ import Link from '@mui/material/Link'
 import Image from 'next/image'
 import homePageImagePaths from '../../../constants/ImagePaths/Homepage/homePageImagePaths'
 import demosection from '@/constants/Homepage/demosection'
+import { motion } from 'framer-motion'
 const DemoSection = () => {
   console.log(demosection)
+  const singlesec = {
+    offscreen: {
+      opacity: 0,
+      rotate: 0,
+      y: 200,
+    },
+    onscreen: {
+      opacity: [0.5, 0.6, 0.7, 0.8, 0.9, 1],
+      rotate: [0, 20, -20, 20, 0],
+      y: 0,
+      transition: {
+        type: 'spring',
+        duration: 10,
+        ease: 'easeInOut',
+        damping: 9.8,
+        stiffness: 100,
+      },
+    },
+  }
   return (
     <Box
       sx={{
@@ -23,33 +44,51 @@ const DemoSection = () => {
       <Box
         sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
       >
-        <Typography
-          variant="h1"
-          sx={{
-            color: 'primary.purp',
-            lineHeight: '1.0',
-            fontSize: {
-              xs: '25px',
-              md: 'clamp(40px, 3vw, 100px)',
-              lg: '100px',
-            },
-          }}
-        >
-          {demosection.title}
-        </Typography>
-        <Typography
-          variant="body1"
-          sx={{
-            margin: '20px 0',
-            fontSize: {
-              xs: 'clamp(20px, 1.5vw, 28px)',
-              md: 'clamp(20px, 1.5vw, 28px)',
-              lg: '28px',
-            },
-          }}
-        >
-          {demosection.description}
-        </Typography>
+        <motion.div transition={{ staggerChildren: 1.9 }}>
+          <motion.div
+            variants={singlesec}
+            initial={'offscreen'}
+            whileInView={'onscreen'}
+            viewport={{ once: true }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Typography
+                variant="h1"
+                sx={{
+                  color: 'primary.purp',
+                  lineHeight: '1.0',
+                  fontSize: {
+                    xs: '25px',
+                    md: 'clamp(50px, 4vw, 64px)',
+                    lg: 'clamp(50px, 4vw, 64px)',
+                  },
+                }}
+              >
+                {demosection.title}
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  margin: '20px 0',
+                  fontSize: {
+                    xs: 'clamp(20px, 1.5vw, 28px)',
+                    md: 'clamp(20px, 1.5vw, 28px)',
+                    lg: '28px',
+                  },
+                }}
+              >
+                {demosection.description}
+              </Typography>
+            </Box>
+          </motion.div>
+        </motion.div>
+
         <Box sx={{ display: 'flex', gap: 5 }}>
           <Box
             sx={{
