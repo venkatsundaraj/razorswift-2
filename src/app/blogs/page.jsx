c9b9d613baf7e51b577826eeb3be8038c972e20f
firebrand-labs <vimalraj@firebrandlabs>
2023-10-05 17:28:00 +0530

    over

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Box } from '@mui/system'
 import HeroSection from '@/components/pageBasedComponents/blogs/HeroSection'
 import Futuredsection from '@/components/pageBasedComponents/blogs/Futuredsection'
+import BlogCard from '@/components/pageBasedComponents/blogs/BlogCard'
 const Blogs = () => {
   return (
     <Box component="main" class="Topcontainer">
       <HeroSection />
       <Futuredsection />
+      <BlogCard />
     </Box>
   )
 }
--- src/components/pageBasedComponents/Homepage/DemoSection.jsx
+++ src/components/pageBasedComponents/Homepage/DemoSection.jsx
@@ -115,7 +115,7 @@ const DemoSection = () => {
             </Link>
           </Box>
         </Box>
-        <Box sx={{ marginTop: '-42px' }}>
+        <Box sx={{ marginTop: { xs: '-10px', md: '-42px' } }}>
           <Image
             alt="readytostartnew"
             style={{ width: 'clamp(350px, 54vw, 1000px)', height: 'auto' }}
